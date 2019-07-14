f0bcdde0e77c268e28b6481fd19497982191a2cd
Ryan Zhang <ryanzhangau@gmail.com>
2019-07-14 19:38:39 +1000

    error reducer

---
+++ src/store/reducer/errorReducer.js
@@ -0,0 +1,14 @@
+import { IMAGES } from '../constants/Images';
+const errorReducer = (state = null, action) => {
+  switch (action.type) {
+    case IMAGES.LOAD_FAIL:
+      return action.error;
+    case IMAGES.LOAD:
+    case IMAGES.LOAD_SUCCESS:
+      return null;
+    default:
+      return state;
+  }
+};
+
+export default errorReducer;
